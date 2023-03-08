9682429d4851f44d403c70c08059c4e0eabb4663
Samuael Ketema <lijsamuael@gmail.com>
2023-03-08 12:57:55 +0300

    making correction on the responsiveness of submitReportModal component

--- src/components/submitReportModal.js
+++ src/components/submitReportModal.js
@@ -4,71 +4,106 @@ const Modal = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed z-50 top-0 left-0 w-full h-full overflow-auto bg-black bg-opacity-80">
-      <div className="relative w-10/12 md:w-6/12 mx-auto mt-24 mb-4 overflow-hidden rounded-lg text-white bg-black rounded shadow-lg">
-        <div className="px-16 pt-4 mb-8 w-full h-full rounded-lg">
-          <h2 className="text-lg font-semibold mb-8 text-center">
+    <div className=" fixed z-50 top-0 left-0 w-full h-full overflow-auto bg-black bg-opacity-80">
+      <div className="relative w-full lg:w-7/12 mx-auto mt-24 mb-4 overflow-hidden rounded-lg text-white bg-black rounded shadow-lg">
+        <div className="px-5 sm:px-10 md:px-20 pt-4 mb-8 w-full h-full rounded-lg">
+          <h2
+            className="text-lg font-bold mb-8 text-center font-sans text-xl"
+            style={{ fontSize: 32 }}
+          >
             Submit Report
           </h2>
 
-          <div className="flex flex-col md:flex-row items-center px-3 rounded-lg border-2 border-gray-500">
-            <div className="py-4">
+          <div className="grid sm:grid-cols-[150px_auto] grid-cols-[100px_auto] items-center pl-3 rounded-lg border-2 border-gray-500">
+            <div className="left-image py-4 ">
               <img
-                src="./images/reportlogo.jpg"
+                src="./images/profile.jpg"
                 alt="report"
-                className="rounded-lg object-cover md:h-36 md:w-32 w-full"
+                className="rounded-lg aspect-h-4 aspect-w-3 object-cover "
               />
             </div>
-            <div className="px-3 py-4">
-              <h3 className="text-lg font-semibold mb-2">Art Title</h3>
-              <p className=" mb-2">
+            <div className=" ml-4 flex flex-col jusitify-between">
+              <div>
+              <h3 className="font-bold mb-2" style={{ fontSize: 22 }}>
+                Art Title
+              </h3>
+              <p
+                className="mb-2 font-light"
+                style={{ fontSize: window.innerWidth < 400 ? "10px" : "14px" }}
+              >
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Blanditiis, asperiores repellat ratione nemo
+                Blanditiis, asperiores repellat ratione nemo aldskjflsdk flskd
               </p>
-              <div className="flex ">
+              </div>
+              <div className="hidden sm:flex flex col-span-2 pb-2  ">
                 <img
                   src="./images/profile.jpg"
-                  width="50"
-                  height="50"
+                  width="60"
+                  height="60"
                   alt="icon"
-                  className="mr-2 rounded-lg"
+                  className="mr-2 rounded-lg profile-img"
                 />
-                <div className="text-sm text-gray-500">
-                  <h3 className="text-lg text-white font-semibold">
+                <div className="text-gray-500 align-bottom">
+                  <h3
+                    className="text-white font-medium"
+                    style={{ fontSize: 20 }}
+                  >
                     Artist Name
                   </h3>
-                  <p>@john_doe</p>
+                  <p
+                    className="text-base text-xl text-sm"
+                    style={{ fontSize: 13 }}
+                  >
+                    @john_doe
+                  </p>
                 </div>
+              </div>
+            </div>
+            <div className="block sm:hidden flex col-span-2 pb-2">
+              <img
+                src="./images/profile.jpg"
+                alt="icon"
+                className="mr-2  rounded-lg profile-img object-cover h-12 w-12"
+              />
+              <div className="text-sm text-gray-500 w-full">
+                <h3 className="text-lg text-white font-semibold">
+                  Artist Name
+                </h3>
+                <p className="text-base text-xl text-sm">@john_doe</p>
               </div>
             </div>
           </div>
 
-          <div className="flex flex-wrap items-center justify-start mt-12 ">
-            <button className="px-6 mb-6 h-8 font-semibold text-white bg-gray-700 rounded hover:bg-gray-500 mr-5">
+          <div className="flex flex-wrap items-center justify-start">
+            <button className="font-sans px-6 h-8 text-sm text-white bg-gray-700 rounded hover:bg-gray-500 mr-5" style={{height: 40, borderRadius: 10, marginTop:36}}>
               Reporter
             </button>
-            <button className="mb-6  flex fled-nowrap px-2  h-8 font-semibold text-white bg-gray-700 rounded hover:bg-gray-500">
+            <button className=" flex flex-nowrap items-center px-2  text-sm text-white bg-gray-700 rounded hover:bg-gray-500" style={{height: 40, borderRadius: 10, marginTop:36}}>
               <img
                 src="./images/profile.jpg"
                 alt="email"
-                className="mr-2 inline-block h-8 w-8 rounded-lg"
+                className="inline-block aspect-h-4  aspect-w-3 h-10 "
+                style={{paddingLeft:10, paddingRight:10, paddingTop:8, paddingBottom:8, borderRadius: 15}}
               />
-              example@gmail.com
+              <span className="inline-block align-middle">
+                example@gmail.com
+              </span>
             </button>
           </div>
-          <p className=" pb-4 font-semibold">Message</p>
+          <p className=" font-semibold" style={{ marginTop:36}}>Message</p>
           <textarea
-            className="w-full h-64 px-3 py-2 mb-6 rounded-md bg-gray-700 "
+            className="w-full h-64 px-3  rounded-md bg-gray-700 "
             rows="5"
             placeholder="Please give us a detail report on why reporting this art..."
+            style={{ marginTop:18}}
           ></textarea>
 
-          <div className="flex justify-end">
+          <div className="flex justify-center sm:justify-end">
             <button
               className="w-32 py-2 mr-5 font-semibold text-black bg-white rounded hover:bg-gray-200 focus:bg-gray-200"
               onClick={onClose}
             >
-              Back
+              Cancel
             </button>
             <button className="w-32 py-2 font-semibold text-white bg-orange-500 rounded hover:bg-orange-600 focus:bg-orange-600">
               Submit
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
+    fontSize: {
+      ssm: "0.8125rem",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
