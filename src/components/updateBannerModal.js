f19f2cbaad92edbf3a21c256b8519ef0bc201d47
Samuael Ketema <lijsamuael@gmail.com>
2023-03-09 20:37:29 +0300

    (fix) image of image upload component fixed

--- src/components/updateBannerModal.js
+++ src/components/updateBannerModal.js
@@ -12,7 +12,7 @@ const UpdateBannerModal = ({ isOpen, onClose }) => {
           </h2>
           <div className="flex flex-col  space-y-[21px]">
           <div class=" flex flex-col space-y-[8px]">
-            <p className="font-medium text-xl">Title:</p>
+            <p className="font-bold text-xl">Title:</p>
 
               <input
                 className="w-full max-w-[1200px] h-[67px] px-3 py-4 rounded-md  text-xl font-normal text-gray-700 bg-gray-700"
@@ -39,22 +39,8 @@ const UpdateBannerModal = ({ isOpen, onClose }) => {
                   for="dropzone-file"
                   class="flex flex-col items-center justify-center w-full h-[380px] border-[1px] border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-700 hover:bg-gray-600"
                 >
-                  <div class="flex flex-col items-center justify-center pt-5 pb-6 space-y-[23px]">
-                    <svg
-                      aria-hidden="true"
-                      class="w-10 h-10 mb-3 text-gray-400"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
-                      ></path>
-                    </svg>
+                  <div class="flex flex-col items-center justify-center pt-5 pb-6 space-y-[11px]">
+                    <img src="./images/photo.png" alt="uploadimage" className="h-[150px]" />
                     <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
                       <span class="font-bold text-xl">Upload Image</span>
                     </p>
