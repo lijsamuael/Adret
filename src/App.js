5823f6dbc5c3b5d4910c6c59e3d7ba401a47680c
Samuael Ketema <lijsamuael@gmail.com>
2023-03-09 14:42:40 +0300

    (fix) fix on the text of buttons in app.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
   return (
     <div>
       <button className="px-4 py-2 font-semibold text-white bg-blue-500 rounded hover:bg-blue-600 focus:bg-blue-600" onClick={handleOpenReportModal}>Open Submit Report Modal</button>
-      <button className="px-4 py-2 font-semibold text-white bg-blue-500 rounded hover:bg-blue-600 focus:bg-blue-600" onClick={handleOpenUpdateBannerModal}>Open Submit Report Modal</button>
+      <button className="px-4 py-2 font-semibold text-white bg-blue-500 rounded hover:bg-blue-600 focus:bg-blue-600" onClick={handleOpenUpdateBannerModal}>Open Update Banner Modal</button>
       <Modal isOpen={reportModalIsOpen} onClose={handleCloseReportModal} />
       <UpdateBannerModal isOpen={updateBannerModalIsOpen} onClose={handleCloseUpdateBannerModal} />
 
