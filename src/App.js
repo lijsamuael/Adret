6a86baa598f3661a58e50422044bd27cf34e1fcd
Samuael Ketema <lijsamuael@gmail.com>
2023-03-10 16:27:32 +0300

    (fix) making the heifght of the price table  equal to screen size.

--- src/App.js
+++ src/App.js
@@ -25,10 +25,10 @@ const App = () => {
 
   return (
     <div>
-      <button className="px-4 py-2 font-semibold text-white bg-blue-500 rounded hover:bg-blue-600 focus:bg-blue-600" onClick={handleOpenReportModal}>Open Submit Report Modal</button>
+      {/* <button className="px-4 py-2 font-semibold text-white bg-blue-500 rounded hover:bg-blue-600 focus:bg-blue-600" onClick={handleOpenReportModal}>Open Submit Report Modal</button>
       <button className="px-4 py-2 font-semibold text-white bg-blue-500 rounded hover:bg-blue-600 focus:bg-blue-600" onClick={handleOpenUpdateBannerModal}>Open Update Banner Modal</button>
       <Modal isOpen={reportModalIsOpen} onClose={handleCloseReportModal} />
-      <UpdateBannerModal isOpen={updateBannerModalIsOpen} onClose={handleCloseUpdateBannerModal} />
+      <UpdateBannerModal isOpen={updateBannerModalIsOpen} onClose={handleCloseUpdateBannerModal} /> */}
       <PriceTable/>
 
 
--- src/components/priceTable.js
+++ src/components/priceTable.js
@@ -13,17 +13,19 @@ function PriceTable() {
   };
 
   return (
-    <div className="bg-black text-white  h-full">
-      <div className="flex flex-col content-center w-full max-w-[1890px] px-[10px]  sm:px-[20px] md:px-[50px] lg:px-[70px] xl:px-[150px] 2xl:px-[232px] py-[170px] space-y-[67px] mx-auto">
+    <div className="bg-black text-white h-screen">
+      <div className="flex flex-col content-center w-full max-w-[1890px] px-[10px]  sm:px-[20px] md:px-[50px] lg:px-[70px] xl:px-[150px] 2xl:px-[232px] py-[100px] sm:py-[170px] space-y-[55px] sm:space-y-[67px] mx-auto">
         <div className="space-y-4">
-          <h2 className="text-[50px] font-semibold ">Price Table</h2>
-          <p className="text-[30px] font-semibold">
+          <h2 className="text-[40px] sm:text-[50px] font-semibold ">
+            Price Table
+          </h2>
+          <p className="text-[25px] sm:text-[30px] font-semibold">
             Price and size information for Lorem ipsum
           </p>
         </div>
-        <table className=" w-full mx-auto table-fixed text-center">
+        <table className=" w-full mx-auto table-fixed text-center ">
           <thead className="h-28 ">
-            <tr className=" bg-gray-700">
+            <tr className=" bg-gray-700 ">
               <th className="font-medium text-xl">No.</th>
               <th className="font-medium text-xl">Size</th>
               <th className="font-medium text-xl">Price</th>
@@ -32,12 +34,12 @@ function PriceTable() {
             </tr>
           </thead>
           <tbody className="h-96">
-            <tr className="border-b border-b-gray-700">
+            <tr className="border-b border-b-gray-700 ">
               <td>1.</td>
               <td>12x13</td>
               <td>100</td>
               <td>100</td>
-              <td className=""  onClick={handleOpenUpdatePriceModal}>
+              <td className="" onClick={handleOpenUpdatePriceModal}>
                 <img src="./images/edit-2.png" alt="" className="mx-auto" />
               </td>
             </tr>
